f6ed757382ac4edb54e55e02e5cdf9a91915772e
Vitaly <72764133+vitlyc@users.noreply.github.com>
2022-09-03 17:03:52 +0300

    add Document Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
