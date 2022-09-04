de13eb88de32b876a3dfc637c87d7a0b676dd0e4
Vitaly <72764133+vitlyc@users.noreply.github.com>
2022-09-04 15:19:36 +0300

    add some comps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
-// import React from "react";
-import { Button, Htag } from "../components";
+import React from "react";
+import { Button, Htag, P, Tag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
-      <Htag tag="h3">Текст</Htag>
-      <Button appearance="primary">Кнопка</Button>
-      <Button appearance="ghost">Кнопка</Button>
+      <Htag tag="h1">Текст</Htag>
+      <Button appearance="primary" arrow="right">
+        Кнопка
+      </Button>
+      <Button appearance="ghost" arrow="down">
+        Кнопка
+      </Button>
+      <P size="l">Большой</P>
+      <P>Средний</P>
+      <P size="s">Маленький</P>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">
+        Red
+      </Tag>
+      <Tag size="s" color="green">
+        Green
+      </Tag>
+      <Tag color="primary">Green</Tag>
     </>
   );
 }
