17d7061248f952e44158ccece78347761048c035
Vitaly <72764133+vitlyc@users.noreply.github.com>
2022-10-17 17:36:46 +0300

    add context

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,24 +1,26 @@
 import styles from './Menu.module.css';
 import cn from 'classnames';
-import { useContext } from 'react';
+import { format } from 'date-fns';
+import { useContext, useEffect } from 'react';
 import { AppContext } from '../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
+import CoursesIcon from './icons/courses.svg';
+import ServicesIcon from './icons/services.svg';
+import BooksIcon from './icons/books.svg';
+import ProductsIcon from './icons/products.svg';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+import { P } from '../../components';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { firstLevelMenu } from '../../helpers/helpers';
+
+const firstLevelMenu: FirstLevelMenuItem[] = [
+	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
+	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
+	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products }
+];
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
-	const router = useRouter();
-
-	const openSecondLevel = (secondCategory: string) => {
-		setMenu && setMenu(menu.map(m => {
-			if (m._id.secondCategory == secondCategory) {
-				m.isOpened = !m.isOpened;
-			}
-			return m;
-		}));
-	};
 
 	const buildFirstLevel = () => {
 		return (
@@ -45,21 +47,16 @@ export const Menu = (): JSX.Element => {
 	const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
 		return (
 			<div className={styles.secondBlock}>
-				{menu.map(m => {
-					if (m.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
-						m.isOpened = true;
-					}
-					return (
-						<div key={m._id.secondCategory}>
-							<div className={styles.secondLevel} onClick={() => openSecondLevel(m._id.secondCategory)}>{m._id.secondCategory}</div>
-							<div className={cn(styles.secondLevelBlock, {
-								[styles.secondLevelBlockOpened]: m.isOpened
-							})}>
-								{buildThirdLevel(m.pages, menuItem.route)}
-							</div>
+				{menu.map(m => (
+					<div key={m._id.secondCategory}>
+						<div className={styles.secondLevel}>{m._id.secondCategory}</div>
+						<div className={cn(styles.secondLevelBlock, {
+							[styles.secondLevelBlockOpened]: m.isOpened
+						})}>
+							{buildThirdLevel(m.pages, menuItem.route)}
 						</div>
-					);
-				})}
+					</div>
+				))}
 			</div>
 		);
 	};
@@ -69,7 +66,7 @@ export const Menu = (): JSX.Element => {
 			pages.map(p => (
 				<Link href={`/${route}/${p.alias}`}>
 					<a className={cn(styles.thirdLevel, {
-						[styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath
+						[styles.thirdLevelActive]: false
 					})}>
 						{p.category}
 					</a>
--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,13 +2,10 @@ import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.css';
 import cn from 'classnames';
 import { Menu } from '../Menu/Menu';
-import Logo from '../logo.svg';
 
-export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
 	return (
-		<div className={cn(className, styles.sidebar)} {...props}>
-			<Logo className={styles.logo} />
-			<div>поиск</div>
+		<div {...props}>
 			<Menu />
 		</div>
 	);
